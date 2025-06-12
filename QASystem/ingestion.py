7d491d269855dfd9ef4c02247c28a43b7749c372
AtharvaBOT7 <atharvachundurwar841@gmail.com>
2025-06-12 16:41:03 -0700

    changes to gitignore and import statements added to ingestion.py file

---
+++ QASystem/ingestion.py
@@ -0,0 +1,10 @@
+from haystack import Pipeline
+from haystack.components.writers import DocumentWriter
+from haystack.components.preprocessors import DocumentSplitter
+from haystack.components.embedders import SentenceTransformersDocumentEmbedder
+from haystack.integrations.document_stores.pinecone import PineconeDocumentStore
+from haystack.components.converters import PyPDFToDocument
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+from QASystem.utility import pinecone_config
