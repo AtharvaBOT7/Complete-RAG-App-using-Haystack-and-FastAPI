060a26e189cc83147d4996088b6e5d79e17ef970
AtharvaBOT7 <atharvachundurwar841@gmail.com>
2025-06-12 20:30:21 -0700

    requirements.txt file updated, code added in ingestion and utils

--- QASystem/ingestion.py
+++ QASystem/ingestion.py
@@ -8,9 +8,24 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-def ingest():
-    pass
+from QASystem.utils import pinecone_config
+
+def ingest(document_store):
+    indexing = Pipeline()
+
+    # Adding components to the Haystack Pipeline
+    indexing.add_component("converter", PyPDFToDocument())
+    indexing.add_component("splitter", DocumentSplitter(split_by="sentence", split_length=2))
+    indexing.add_component("embedder", SentenceTransformersDocumentEmbedder)
+    indexing.add_component("writer", DocumentWriter(document_store))
+
+    indexing.connect("converter", "splitter")
+    indexing.connect("splitter", "embedder")
+    indexing.connect("embedder", "writer")
+
+    indexing.run({"converter": {"sources": [Path("Data/2312.00752v2.pdf")]}})
 
 if __name__=="__main__":
-
+    
+    pinecone_config()
     ingest()
--- QASystem/utils.py
+++ QASystem/utils.py
@@ -7,6 +7,9 @@ load_dotenv()
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 os.environ['PINECONE_API_KEY'] = PINECONE_API_KEY
 
+HF_TOKEN = os.getenv("HF_TOKEN")
+os.environ['HF_API_TOKEN'] = HF_TOKEN
+
 print("Key imported successfully")
 
 from pinecone import Pinecone, ServerlessSpec
@@ -15,3 +18,11 @@ pc = Pinecone(api_key = PINECONE_API_KEY)
 
 print("Available indexes:", pc.list_indexes())
 
+def pinecone_config():
+    document_store = PienconeDocumentStore(
+        environment="gcp-starter",
+        index="haystackfastapi",
+        namespace="default",
+        dimension=768
+    )
+    return document_store
